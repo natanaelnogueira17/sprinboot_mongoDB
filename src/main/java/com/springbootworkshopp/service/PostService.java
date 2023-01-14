e83065de1709a580fba450c73a49b4497f59465a
natanael nogueira <natanaelnogueira019@gmail.com>
2023-01-14 13:14:19 -0300

    consulta com fullsearch

--- src/main/java/com/springbootworkshopp/service/PostService.java
+++ src/main/java/com/springbootworkshopp/service/PostService.java
@@ -1,5 +1,6 @@
 package com.springbootworkshopp.service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,6 +26,11 @@ public class PostService {
 	
 	public List<Post>findByTitle(String text){
 		return repository.findByTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate){
+		maxDate = new Date(maxDate.getTime()+24*60*60*1000);
+		return repository.fullSearch(text, minDate, maxDate);
 	}
 
 }
